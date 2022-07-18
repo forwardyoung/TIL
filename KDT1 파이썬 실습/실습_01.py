53f524066520e72ce2e2a9bbf6bbbcc5296196d3
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2022-07-18 17:56:48 +0900

    Delete KDT1 파이썬 실습 directory

--- KDT1 파이썬 실습/실습_01.py
+++
@@ -1,6 +0,0 @@
-num = int(input())
-if num % 2 == 0:
-    if num % 3 == 0:
-        print('참')
-else:
-    print('거짓')
--- KDT1 파이썬 실습/실습_06.py
+++
@@ -1,9 +0,0 @@
-# 리스트에서 최댓값 구하기
-numbers = [0, 20, 100]
-max = numbers[0] # 임의로 가장 앞의 값을 max 변수에 넣어두고 리스트가 돌아가면서 하나씩 비교!
-
-for num in numbers:
-    if max < num:
-        # 만약, max 값이 num보다 작으면 바꾼다.
-        max = num
-print(max)
