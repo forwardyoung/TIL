67ab09951be9a679284fe001750cafdf095f3439
Cha Hwa Young <108648684+forwardyoung@users.noreply.github.com>
2022-07-21 14:48:40 +0900

    Delete SWEA directory

--- SWEA/2050.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.stdin = open("input_2050.txt", "r")
-t = input()
-l = list(t)
-result = ''.join(map(str, l))
-for i in result:
-    b= ord(i)-64
-    print(b, end=' ')
--- SWEA/D1/1933.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.stdin = open("input_1933.txt", "r")
-# 정수 N 의 모든 약수를 오름차순으로 출력한다.
-N = int(input())
-for i in range(1, N+1):
-    if N % i ==0:
-        # 정수 N을 i(1~N까지)로 나눌 때 나머지가 0이면 그 때 i는 약수이다.
-        print(i, end=' ')
