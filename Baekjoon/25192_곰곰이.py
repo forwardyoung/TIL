4a70af2895460939c08a2bc1770353b6fdbac5f5
forwardyoung <chahwayoung214@gmail.com>
2022-08-03 09:48:10 +0900

    25192_수정

--- Baekjoon/25192_곰곰이.py
+++ Baekjoon/25192_곰곰이.py
@@ -1,5 +1,6 @@
+# 풀이 1
 import sys
-input = sys.stdin.readline("25192.txt")
+input = sys.stdin.readline
 
 N = int(input())
 cnt = 0 # 곰곰티콘 사용한 횟수를 0으로 초기화
@@ -15,3 +16,21 @@ for _ in range(N):
         user.add(chat) # ENTER가 아니면 chat을 user에 더한다.
 cnt += len(user) # user에 있는 사람의 수를 cnt에 더한다.
 print(cnt)
+
+# 풀이 2
+N = 7
+gom = 0 # 곰곰티콘 사용 횟수
+log_list = ["ENTER", "pjshwa", "chansol", "chogahui05", "ENTER", "pjshwa", "chansol"]
+set_ = set()
+for log in log_list :
+    if log == "ENTER": # log가 ENTER라면
+        set_.clear() # 기존에 있던 것을 clear로 지워준다.
+
+    else: # log가 ENTER가 아닐 때
+    # 닉네임 = log
+    # 리스트에서 중복을 탐색할 때는 N만큼의 시간이 필요함.
+    # set에서 중복을 탐색할 때는 1만큼의 시간이 필요함!
+        if log not in set_: # 닉네임이 set_에 없을 때만 
+            set_.add(log) # 새로운 닉네임을 set_에 저장하고
+            gom += 1 # 곰곰티콘 횟수 + 1
+print(gom)
