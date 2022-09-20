31b26303ecda1df5472f8c4c971e772da6693832
forwardyoung <chahwayoung214@gmail.com>
2022-09-20 14:12:46 +0900

    0919

--- WEB/Web_practice/0919/form.js
+++ WEB/Web_practice/0919/form.js
@@ -3,8 +3,9 @@ const form = document.querySelector('#form')
         e.preventDefault()
         // FormData 객체는 요소를 form
         const formData = new FormData(form)
-        console.log(formData) // (파이썬에서의 map object 같은) 별도의 객체여서 조회 X. 반복하면됨
+        console.log(formData) // (파이썬에서의 map object 같은) 별도의 객체여서 조회 X. 반복하면 됨
         // formData.get(name) => 주어진 name 의 해당하는 필드 value를 반환
         console.log(formData.get('password'))
         console.log(formData.get('password_confirmation'))
       })
+      
